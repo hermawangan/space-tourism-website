b7766481e39d781e9944ad386e9849464bf49f71
Hermawan <hermawangan39@gmail.com>
2022-04-12 17:26:31 +0700

    style fully implemented

--- src/Components/Destination.js
+++ src/Components/Destination.js
@@ -12,27 +12,41 @@ function Destination({ destination, errorMsg, fetchDestination }) {
   const clickHandler = (e) => {
     setPlanetMoon(e.target.id);
   };
+  // text-[16px] leading-[19px] tracking-[2.7px]
 
   return (
-    <div className="bg-destination-mobile md:bg-destination-tablet lg:bg-destination-desktop text-white">
-      <span>01</span>
-      <h2> PICK YOUR DESTINATION </h2>
-      <div>
-        <div>
-          {destination.map((moon) =>
-            moon.name === planetMoon ? (
-              <img key={moon.distance} src={moon.images.png} alt="planet" />
-            ) : null
-          )}
+    <div className="bg-destination-mobile md:bg-destination-tablet lg:bg-destination-desktop text-white flex flex-col items-center text-center  lg:flex-row lg:justify-evenly lg:text-left ">
+      <div className="lg:mb-[110px] md:w-full">
+        <div className=" flex mt-[88px] text-[16px] leading-[19px] tracking-[2.7px]  md:w-full md:justify-start md:pl-[76.5px] md:mt-[136px] md:text-[20px] md:leading-[24px] md:tracking-[3.375px] lg:pl-0 lg:mt-[212px] lg:text-[28px] lg:leading-[44px] lg:tracking-[4.725px]  lg:pl-[166.5px] ">
+          <span className="mr-[18px] font-bold">01</span>
+          <h2> PICK YOUR DESTINATION </h2>
         </div>
-        <div>
-          <ul>
+
+        <div className="mt-8 lg:mt-20">
+          <div className="flex justify-center ">
+            {destination.map((moon) =>
+              moon.name === planetMoon ? (
+                <img
+                  key={moon.distance}
+                  src={moon.images.png}
+                  alt="planet"
+                  className="w-[170px] h-[170px] md:w-[300px] md:h-[300px] lg:w-[445px] lg:h-[445px]"
+                />
+              ) : null
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="lg:mt-[310px] lg:w-[445px] lg:h-[445px] lg:mr-[163px]">
+        <div className="flex justify-center lg:justify-start">
+          <ul className="flex justify-around mt-[26px] text-neutral-lightpurple text-[14px] leading-[17px] tracking-[2.36px] w-[237.5px] h-7 md:w-[285.5px] md:h-9 md:text-[16px] md:leading-[19px] md:tracking-[2.7px] lg:mt-0">
             <li
               id="Moon"
               onClick={clickHandler}
               className={`${
-                planetMoon === "Moon" ? "border-2 border-b-solid" : null
-              }`}
+                planetMoon === "Moon" ? "border-b-2 border-b-white" : null
+              } hover:border-b-2 hover:opacity-50 cursor-pointer`}
             >
               Moon
             </li>
@@ -40,8 +54,8 @@ function Destination({ destination, errorMsg, fetchDestination }) {
               id="Mars"
               onClick={clickHandler}
               className={`${
-                planetMoon === "Mars" ? "border-2 border-b-solid" : null
-              }`}
+                planetMoon === "Mars" ? "border-b-2 border-b-white" : null
+              } hover:border-b-2 hover:opacity-50 cursor-pointer`}
             >
               Mars
             </li>
@@ -49,8 +63,8 @@ function Destination({ destination, errorMsg, fetchDestination }) {
               id="Europa"
               onClick={clickHandler}
               className={`${
-                planetMoon === "Europa" ? "border-2 border-b-solid" : null
-              }`}
+                planetMoon === "Europa" ? "border-b-2 border-b-white" : null
+              } hover:border-b-2 hover:opacity-50 cursor-pointer`}
             >
               Europa
             </li>
@@ -58,31 +72,45 @@ function Destination({ destination, errorMsg, fetchDestination }) {
               id="Titan"
               onClick={clickHandler}
               className={`${
-                planetMoon === "Titan" ? "border-2 border-b-solid" : null
-              }`}
+                planetMoon === "Titan" ? "border-b-2 border-b-white" : null
+              } hover:border-b-2 hover:opacity-50 cursor-pointer`}
             >
               Titan
             </li>
           </ul>
-          {destination.map((moon) => {
-            return moon.name === planetMoon ? (
-              <div key={moon.distance}>
-                <h1>{moon.name}</h1>
-                <p>{moon.description}</p>
-                <div>
-                  <div>
-                    <p>Avg. Distance</p>
-                    <span>{moon.distance}</span>
-                  </div>
-                  <div>
-                    <p>EST. TRAVEL TIME</p>
-                    <span>{moon.travel}</span>
-                  </div>
+        </div>
+
+        {destination.map((moon) => {
+          return moon.name === planetMoon ? (
+            <div key={moon.distance} className="mt-[20px] lg:mt-0">
+              <h1 className="text-[56px] leading-[64px] md:text-[80px] md:leading-[92px] lg:text-[100px] lg:leading-[115px] ">
+                {moon.name.toUpperCase()}
+              </h1>
+              <p className="text-neutral-lightpurple text-[15px] w-[327px] h-[125px] mb-8 md:text-[16px] md:leading-[28px] md:w-[573px] md:h-[84px] md:mt-2 lg:text-[18px] lg:leading-[32px] lg:w-[444px] lg:h-[128px] lg:mt-3.5">
+                {moon.description}
+              </p>
+              <div className="border-b-2 border-neutral-lightdark w-[327px]  mb-8 md:w-[573px] lg:w-[444px]"></div>
+              <div className="md:flex justify-evenly items-center mb-[58px] lg:justify-start">
+                <div className="lg:mr-[77px]">
+                  <p className="text-neutral-lightpurple text-[14px] leading-[17px] tracking-[2.3625px] md:pb-3">
+                    Avg. Distance
+                  </p>
+                  <span className="text-[28px] leading-[32px]">
+                    {moon.distance.toUpperCase()}
+                  </span>
+                </div>
+                <div className="mt-8 md:mt-0">
+                  <p className="text-neutral-lightpurple text-[14px] leading-[17px] tracking-[2.3625px] md:pb-3">
+                    EST. TRAVEL TIME
+                  </p>
+                  <span className="text-[28px] leading-[32px]">
+                    {moon.travel.toUpperCase()}
+                  </span>
                 </div>
               </div>
-            ) : null;
-          })}
-        </div>
+            </div>
+          ) : null;
+        })}
       </div>
     </div>
   );
@@ -102,3 +130,5 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Destination);
+
+// flex mt-[88px] text-[16px] leading-[19px] tracking-[2.7px]border-2 border-green-700
